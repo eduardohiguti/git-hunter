1689e1dff85106f03813a403b8a78b82732f3ba3
Eduardo Higuti <eduardohiguti@outlook.com>
2025-07-01 15:50:04 -0300

    adicionado comando commit

--- main.go
+++ main.go
@@ -6,6 +6,14 @@ import (
 	"fmt"
 	"os"
 	"path/filepath"
+	"time"
+	"strings"
+)
+
+const (
+	repoObjectsDir = "repo_objects"
+	repoCommitsDir = "repo_commits"
+	currentSnapshotFile = ".hunter_snapshot"
 )
 
 func addToRepository(filePath string) (string, error) {
@@ -32,14 +40,85 @@ func addToRepository(filePath string) (string, error) {
 
 	fmt.Printf("Conteúdo de '%s' adicionado ao 'repositório' com hash: %s\n", filePath, hash)
 
+	snapshotEntry := fmt.Sprintf("%s %s\n", hash, filePath)
+	f, err := os.OpenFile(currentSnapshotFile, os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
+	if err != nil {
+		return "", fmt.Errorf("erro ao abrir arquivo de snapshot: %w", err)
+	}
+	defer f.Close()	
+	if _, err := f.WriteString(snapshotEntry); err != nil {
+		return "", fmt.Errorf("erro ao escrever no arquivo de snapshot: %w", err)
+	}
+
 	return hash, nil
+}
+
+func commitChanges(message string) error {
+	snapshotContent, err := os.ReadFile(currentSnapshotFile)
+	if os.IsNotExist(err) || len(snapshotContent) == 0 {
+		return fmt.Errorf("não há arquivos para comitar. Use 'hunter add <arquivo>' primeiro.")
+	}
+	if err != nil {
+		return fmt.Errorf("erro ao ler snapshot atual: %w", err)
+	}
+
+	commitId := sha1.New()
+	commitId.Write([]byte(message))
+	commitId.Write(snapshotContent)
+
+	commitId.Write([]byte(time.Now().Format(time.RFC3339Nano)))
+
+	commitHash := hex.EncodeToString(commitId.Sum(nil))
+
+	commitContent := fmt.Sprintf(
+		"commit %s\n"+
+		"data: %s\n"+
+		"\n"+
+		"	%s\n"+
+		"\n"+
+		"Arquivos:\n%s",
+		commitHash,
+		time.Now().Format(time.RFC3339Nano),
+		message,
+		string(snapshotContent),
+	)
+
+	err = os.MkdirAll(repoCommitsDir, 0755)
+	if err != nil {
+		return fmt.Errorf("erro ao criar diretório de commits: %w", err)
+	}
+
+	commitPath := filepath.Join(repoCommitsDir, commitHash)
+	err = os.WriteFile(commitPath, []byte(commitContent), 0644)
+	if err != nil {
+		return fmt.Errorf("erro ao salvar objeto commmit: %w", err)
+	}
+
+	fmt.Printf("Commit '%s' criado com sucesso! Hash: %s\n", message, commitHash)
+
+	err = os.WriteFile(currentSnapshotFile, []byte{}, 0644)
+	if err != nil {
+		fmt.Printf("Aviso: erro ao limpar arquivo de snapshot: %v\n", err)
+	}
+
+	return nil
 }
 
 func main() {
 	args := os.Args[1:]
 
 	if len(args) == 0 {
-		fmt.Println("Hunter versionamento de arquivos")
+		fmt.Println("Hunter é uma ferramenta para versionamento de arquivos")
+		fmt.Println()
+		fmt.Println("Uso:")
+		fmt.Println()
+		fmt.Println("	hunter <comando> [argumentos]")
+		fmt.Println()
+		fmt.Println("Os comandos são:")
+		fmt.Println()
+		fmt.Println("	add		Adiciona o arquivo para o repositório")
+		fmt.Println("	commit		Cria um novo commit com os arquivos no 'snapshot'")
+		fmt.Println()
 		return
 	}
 
@@ -56,7 +135,17 @@ func main() {
 		if err != nil {
 			fmt.Println("Erro:", err)
 		}
-	default: 
+	case "commit":
+		if len(args) < 2 {
+			fmt.Println("Uso: commit \"<mensagem-do-commit>\"")
+			return
+		}
+		commitMessage := strings.Join(args[1:], " ") 
+		err := commitChanges(commitMessage)
+		if err != nil {
+			fmt.Println("Erro:", err)
+		}
+ 	default: 
 		fmt.Println("Comando não reconhecido:", args[0])
 	}
 }
